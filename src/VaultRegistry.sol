d8f6c72b9c52c416a767674ce95d6ed8dc1a5bbf
ExoticSolutions <1exoticeditz@gmail.com>
2026-09-24 13:35:04 -0400

    updates almost doen with registry lots added added quoter added multichain dpeloyment gotta work on multi chain quoter and tiny functions we need in registry then add strategy and boom we got this dont give up

--- src/VaultRegistry.sol
+++ src/VaultRegistry.sol
@@ -1,47 +1,33 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.24;
-import {
-    OAppOptionsType3
-} from "@layerzerolabs/oapp-evm/contracts/oapp/libs/OAppOptionsType3.sol";
+import {OAppOptionsType3} from "@layerzerolabs/oapp-evm/contracts/oapp/libs/OAppOptionsType3.sol";
 
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import {
-    ERC4626
-} from "@openzeppelin/contracts/token/ERC20/extensions/ERC4626.sol";
+import {ERC4626} from "@openzeppelin/contracts/token/ERC20/extensions/ERC4626.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import {
-    ReadCodecV1,
-    EVMCallRequestV1
-} from "@layerzerolabs/oapp-evm/contracts/oapp/libs/ReadCodecV1.sol";
+import {ReadCodecV1, EVMCallRequestV1} from "@layerzerolabs/oapp-evm/contracts/oapp/libs/ReadCodecV1.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
-import {
-    OApp,
-    Origin,
-    MessagingFee
-} from "@layerzerolabs/oapp-evm/contracts/oapp/OApp.sol";
+import {OApp, Origin, MessagingFee} from "@layerzerolabs/oapp-evm/contracts/oapp/OApp.sol";
 import {VaultHelper} from "./VaultHelper.sol";
 import {MessagingHelper} from "./MessagingHelper.sol";
 import {StrategyHelper} from "./StrategyHelper.sol";
 import {VaultAssets} from "../src/VaultAssets.sol";
 import {VaultStrategies} from "./VaultStrategies.sol";
-import {
-    MessagingReceipt
-} from "@layerzerolabs/oapp-evm/contracts/oapp/OApp.sol";
+import {MessagingReceipt} from "@layerzerolabs/oapp-evm/contracts/oapp/OApp.sol";
 import {VaultManager} from "./VaultManager.sol";
 import {IVaultManager as VaultFactory} from "./IVaultManager.sol";
 
-contract VaultRegistry {
+import {OApp, Origin, MessagingFee} from "@layerzerolabs/oapp-evm/contracts/oapp/OApp.sol";
+
+contract VaultRegistry is OApp {
+    address endpoint;
+    uint32 _endpointId;
     mapping(address => VaultHelper.AvenorUser) avenorUsers;
     mapping(address => VaultHelper.AvenorCreator) avenorCreators;
 
-    constructor(VaultFactory _vaultFactory) {
-        vaultFactory = _vaultFactory;
-    }
+    constructor(VaultFactory _vaultFactory) {}
 
-    function addUser(
-        address _userAddr,
-        VaultHelper.AvenorUser memory _newUser
-    ) public {
+    function addUser(address _userAddr, VaultHelper.AvenorUser memory _newUser) public {
         avenorUsers[_userAddr] = _newUser;
     }
 
@@ -50,15 +36,11 @@ contract VaultRegistry {
         avenorCreators[msg.sender] = creator;
     }
 
-    function getCreator(
-        address creator
-    ) public returns (VaultHelper.AvenorCreator memory) {
+    function getCreator(address creator) public returns (VaultHelper.AvenorCreator memory) {
         return avenorCreators[creator];
     }
 
-    function getUser(
-        address _user
-    ) public view returns (VaultHelper.AvenorUser memory) {
+    function getUser(address _user) public view returns (VaultHelper.AvenorUser memory) {
         return avenorUsers[_user];
     }
 
@@ -73,6 +55,12 @@ contract VaultRegistry {
     }
 
     function deployMultiChainBridge() public {}
+
+    function getMultiChainDpeloymentQuote() public {}
+
+    function getMessageQuote() public {}
+
+    function addVaultPeer() public {}
 
     function mcVaultDeployment() public {}
 
